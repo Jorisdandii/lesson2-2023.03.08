8c4784a2b4f38bbed2d013a9bdc8e15beae02a9b
Sirojiddin <jorisdandii@gmail.com>
2023-04-01 00:00:23 +0900

    first commit

---
+++ main_1.js
@@ -0,0 +1,49 @@
+
+
+let ball = 100;
+
+if(ball >= 86 && ball <=100 ){
+    console.log('A');
+    console.log('tabriklaymiz uqishga kirdingiz');
+}else if (ball >=61 && ball <=85) {
+    console.log('B');
+}else if (ball >=50 && ball <=60) {
+    console.log('C');
+}else {
+    console.log('felse');
+}
+
+let meva = 'olma';
+
+if(meva === 'olma') {
+    console.log('10000');
+}else if (meva === 'nok') {
+    console.log('12000');
+}else if (meva === 'tarvuz') {
+    console.log('20000');
+}else {
+    console.log('bunday meva yuq');
+}
+
+let number = 100;
+
+if(number % 3 == 0 && number % 5 == 0){
+    console.log('3ga va 5ga bolinadi');
+}else if(number % 5 == 0){
+    console.log('5ga bolinadi');
+}else if(number % 3 == 0){
+    console.log('3ga bolinadi');
+}else{
+    console.log('The end');
+}
+
+
+let login = 'login'
+let parol = 'parol'
+if(login === 'login' && parol ==='parol'){
+    console.log('profelingizga xush kelibsiz');
+}else{
+    console.log('parol yoki login xato');
+}
+
+
